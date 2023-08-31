572595aed833b1e5b3e3de7af104afb79e9eea41
Spencer Owens <thespencerowens@outlook.com>
2023-08-30 16:16:32 -0800

    230830

--- hw00-sowens23/hello.cpp
+++ hw00-sowens23/hello.cpp
@@ -1,14 +1,18 @@
 //#include "std_lib_facilities.h"
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
-#include<cmath>
+//#include <stdio.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using std::cout;
 using namespace std;
 inline void keep_window_open() { char ch; cin>>ch; }
 
 int main () // C++ programs start by executing the function main -c PP C++
 {
-	cout<<"Hello, World!\n"; //output "Hello, World!"
+	std::cout<<"Hello, World!\n"; //output "Hello, World!"
 	return 0;
 }
